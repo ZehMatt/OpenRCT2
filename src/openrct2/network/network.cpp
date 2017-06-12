45131e204e6ecb23d8d92f165bffa32130ab515d
Richard Jenkins <rwj@serenity.uk.net>
2017-06-12 13:05:19 +0100

    Implement persistent 'real names of guests' setting

--- src/openrct2/network/network.cpp
+++ src/openrct2/network/network.cpp
@@ -1874,6 +1874,7 @@ bool Network::LoadMap(IStream * stream)
         gCheatsDisablePlantAging = stream->ReadValue<uint8>() != 0;
         gCheatsAllowArbitraryRideTypeChanges = stream->ReadValue<uint8>() != 0;
         gCheatsDisableRideValueAging = stream->ReadValue<uint8>() != 0;
+        gConfigGeneral.show_real_names_of_guests = stream->ReadValue<uint8>() != 0;
 
         gLastAutoSaveUpdate = AUTOSAVE_PAUSE;
         result = true;
@@ -1920,6 +1921,7 @@ bool Network::SaveMap(IStream * stream, const std::vector<const ObjectRepository
         stream->WriteValue<uint8>(gCheatsDisablePlantAging);
         stream->WriteValue<uint8>(gCheatsAllowArbitraryRideTypeChanges);
         stream->WriteValue<uint8>(gCheatsDisableRideValueAging);
+        stream->WriteValue<uint8>(gConfigGeneral.show_real_names_of_guests);
 
         result = true;
     }
