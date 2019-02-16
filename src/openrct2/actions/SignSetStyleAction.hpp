49a3317886bb59e40fb3eda99692062ffff7170c
Matt <m.moninger.h@gmail.com>
2019-02-16 11:28:33 +0100

    Remove faulty cast that would truncate the checked value.

--- src/openrct2/actions/SignSetStyleAction.hpp
+++ src/openrct2/actions/SignSetStyleAction.hpp
@@ -51,7 +51,7 @@ public:
 
     GameActionResult::Ptr Query() const override
     {
-        if ((BannerIndex)_bannerIndex >= MAX_BANNERS || _bannerIndex < 0)
+        if (_bannerIndex >= MAX_BANNERS || _bannerIndex < 0)
         {
             log_warning("Invalid game command for setting sign style, banner id '%d' out of range", _bannerIndex);
             return MakeResult(GA_ERROR::INVALID_PARAMETERS, STR_NONE);
