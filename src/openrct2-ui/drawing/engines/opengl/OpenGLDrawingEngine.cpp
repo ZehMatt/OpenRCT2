d08f591472f1a90d262b9d5a6e0883c49357e4e5
Matt <m.moninger.h@gmail.com>
2020-07-19 14:24:54 +0200

    Filter useless nvidia driver output

--- src/openrct2-ui/drawing/engines/opengl/OpenGLDrawingEngine.cpp
+++ src/openrct2-ui/drawing/engines/opengl/OpenGLDrawingEngine.cpp
@@ -225,9 +225,12 @@ public:
     static void GLAPIENTRY MessageCallback(
         GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
     {
+        if (id == 0x20071)
+            return; // Useless message.
+
         fprintf(
-            stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity, message);
+            stderr, "GL CALLBACK: %s type = 0x%x, id = 0x%x, severity = 0x%x, message = %s\n",
+            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, id, severity, message);
     }
 
     void Initialise() override
