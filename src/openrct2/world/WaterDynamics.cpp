6206d68372c98c4be9171b006232839ac8adf560
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2023-03-26 20:12:59 +0300

    WaterDynamics skeleton

---
+++ src/openrct2/world/WaterDynamics.cpp
@@ -0,0 +1,15 @@
+#include "WaterDynamics.h"
+
+namespace OpenRCT2::World::WaterDynamics
+{
+    void reset()
+    {
+
+    }
+
+    void update()
+    {
+
+    }
+
+} // namespace OpenRCT2::World::WaterDynamics
---
+++ src/openrct2/world/WaterDynamics.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace OpenRCT2::World::WaterDynamics
+{
+    void reset();
+
+    void update();
+    
+} // namespace OpenRCT2::World::WaterDynamics
